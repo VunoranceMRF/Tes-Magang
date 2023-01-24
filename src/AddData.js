56b7a434e17416f6b1642c633716646457c89ace
Razif Febriano <raziffebriano@yahoo.com>
2023-01-25 02:04:40 +0700

    Second Commit

--- src/AddData.js
+++ src/AddData.js
@@ -3,7 +3,21 @@ import "./AddData.css";
 function AddData(){
     return(
         <div className="AddData">
+            <form name="Add Data">
+                Product ID:
+                <input type="text" placeholder="Input Product ID"></input>
+                <br></br>
+                Product Name:
+                <input type="text" placeholder="Input Product Name"></input>
+                <br></br>
+                Customer Name:
+                <input type="text" placeholder="Input Customer Name"></input>
+                <br></br>
+                Transaction Date:
+                <input type="datetime-local" ></input>
+                <br></br>
 
+            </form>
         </div>
     );
 }
